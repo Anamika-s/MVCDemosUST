4f711781e1aa6a306543c73b2d917c0f65d23154
Anamika Sawhney <anamika.sawhney22@gmail.com>
2021-08-26 05:50:26 +0000

    MVC Demos

---
+++ MVCDemos/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemos.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.msg = "Hello";
+            ViewData["message"] = "Passed through ViewData";
+            return View();
+        }
+
+        public IActionResult Method1()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDemos/Controllers/StudentController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemos.Models;
+
+namespace MVCDemos.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Student student = new Student();
+            //student.Id = 1;
+            //student.Name = "Ajay";
+            //student.BatchCode = "B001";
+            //student.Marks = 90;
+
+            // Object Initializer
+
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Ajay",
+                BatchCode = "B001",
+                Marks = 90
+            };
+            ViewBag.student = student;
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            //Student student = new Student();
+            //student.Id = 1;
+            //student.Name = "Ajay";
+            //student.BatchCode = "B001";
+            //student.Marks = 90;
+
+            // Object Initializer
+
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Ajay",
+                BatchCode = "B001",
+                Marks = 90
+            };
+            //ViewBag.student = student;
+            return View(student);
+        }
+
+        //public IActionResult StudentsList()
+        //{
+        //    List<Student> list = new List<Student>();
+
+        //    list.Add(new Student()
+        //    {
+        //        Id = 1,
+        //        Name = "Ajay",
+        //        BatchCode = "B001",
+        //        Marks = 90
+        //    });
+        //    list.Add(new Student()
+        //    {
+        //        Id = 2,
+        //        Name = "Ajay",
+        //        BatchCode = "B001",
+        //        Marks = 90
+        //    });
+        //    return View();
+        //}
+
+        public IActionResult StudentsList()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                 new Student() {Id = 1,
+                Name = "Ajay",
+                BatchCode = "B001",
+                Marks = 90},
+                 new Student() {Id = 2,
+                Name = "Deepak",
+                BatchCode = "B002",
+                Marks = 90 },
+                 new Student(){Id = 3,
+                Name = "Kavya",
+                BatchCode = "B001",
+                Marks = 90 },
+                 new Student(){Id = 4,
+                Name = "Maneesh",
+                BatchCode = "B001",
+                Marks = 90 }
+            };
+            ViewBag.Students = studentList;
+            return View();
+        }
+
+        public IActionResult StudentsList2()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                 new Student() {Id = 1,
+                Name = "Ajay",
+                BatchCode = "B001",
+                Marks = 90},
+                 new Student() {Id = 2,
+                Name = "Deepak",
+                BatchCode = "B002",
+                Marks = 90 },
+                 new Student(){Id = 3,
+                Name = "Kavya",
+                BatchCode = "B001",
+                Marks = 90 },
+                 new Student(){Id = 4,
+                Name = "Maneesh",
+                BatchCode = "B001",
+                Marks = 90 }
+            };
+            //ViewBag.Students = studentList;
+            return View(studentList);
+        }
+    }
+}
